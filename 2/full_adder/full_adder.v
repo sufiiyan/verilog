8b736b6c1445e39c93c2ec2ffd4d241588246d3d
Sufiyan Pasha <97835883+sufiiyan@users.noreply.github.com>
2022-09-19 11:19:24 +0530

    Delete 2 directory

--- 2/full_adder/full_adder.v
+++
@@ -1,6 +0,0 @@
-module full_adder(a,b,c,y,x);
-input a,b,c;
-output y,x;
-assign y = ((!a&!b&c)|(!a&b&c)|(a&!b&!c)|(a&b&c));
-assign x = ((a&b)|(a&c)|(b&c));
-endmodule 
--- 2/simple_circuit/simple_circuit.v
+++
@@ -1,8 +0,0 @@
-module simple_circuit(A,B,C,D,E);
-output D,E;
-input A,B,C;
-wire w1;
-and G1(w1, A, B);
-not G2(E,C);
-or G3(D, w1, E);	
-endmodule
